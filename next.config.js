fd834407635b7be3f8dcd3f24719dfda2dab9191
Vedaangh Rungta <vedaangh.rungta@gmail.com>
2025-01-21 15:53:02 +0000

    c1

---
+++ next.config.js
@@ -0,0 +1,12 @@
+next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'public',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
